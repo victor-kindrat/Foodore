b602456f5ae7869da0c97a37fb4553429f89dda2
Victor Kindrat <kindratvictor07@gmail.com>
2022-02-11 14:01:25 +0200

    menu on the phone and tablets

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let switcherPos = 0;
 let switcherClicks = 1;
 let animDuration = 200;
+let menuTrigger = false;
 
 let screenWidth = $(window).width();
 let screenHeight = $(window).height();
@@ -240,4 +241,48 @@ $('.main__card-btn.main__card-btn-cart').click(function() {
     setTimeout(() => {
         $('.header__my-cart-btn').css('animation', 'none');
     }, 600);
+})
+
+$('#menu-phone').click(function() {
+    if (menuTrigger === false) {
+        $('.header__menu-text').css({
+            'display': 'flex',
+            'flexDirection': 'column',
+            'position': 'fixed',
+            'right': 'calc((var(--cart-height) * 2) + (var(--padding-of-the-page) * 2))',
+            'top': 'calc((var(--padding-of-the-page)) + 10vh)',
+            'background': '#1a1a1a',
+            'borderRadius': '15px',
+            'padding': 'var(--padding-of-the-page)',
+            'boxShadow': '0 0 20px #000000',
+            'rowGap': '25px'
+        });
+        $('.header__menu-btn').css('animation', 'rotate 0.6s 1')
+        setTimeout(() => {
+            $('.header__menu-btn').attr('src', './header/header__mehu-close.svg');
+            $('.header__menu-btn').css('animation', 'none')
+        }, 100);
+        menuTrigger = true;
+    } else if (menuTrigger === true) {
+        console.log('hi');
+        $('.header__menu-text').css({
+            'display': 'none',
+            'flexDirection': 'row',
+            'position': 'relative',
+            'right': '0',
+            'top': '0',
+            'background': 'transparent',
+            'borderRadius': '0',
+            'padding': '0',
+            'boxShadow': 'inherit',
+            'rowGap': '0'
+        });
+        $('.header__menu-btn').css('animation', 'rotate-back 0.6s 1')
+        setTimeout(() => {
+            $('.header__menu-btn').attr('src', './header/header__menu.svg');
+            $('.header__menu-btn').css('animation', 'none')
+        }, 100);
+        menuTrigger = false;
+    }
+    console.log(menuTrigger)
 })
